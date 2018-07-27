bc6dba7e5a7d529e520378507d5be220dea912b5
zhangjianbin <zhangjianbin@jd.com>
2018-07-27 14:27:16 +0800

    添å¾利用mock test 测试授æ获取token

--- oauth2-inmemory-springboot2/src/test/java/com/myke/oauth/test/SecurityOauth2PasswordTest.java
+++ oauth2-inmemory-springboot2/src/test/java/com/myke/oauth/test/SecurityOauth2PasswordTest.java
@@ -3,30 +3,51 @@ package com.myke.oauth.test;
 import com.google.gson.Gson;
 import com.myke.oauth2.AuthCenterApplication;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.codec.binary.Base64;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.*;
-import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.oauth2.common.util.JacksonJsonParser;
+import org.springframework.security.web.FilterChainProxy;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
- * 1. 先启动 AuthCenterApplication 应用
- * 2. 后执行 test
+ * 1. 不用启动 AuthCenterApplication 应用
+ * 2. 直接执行 test
  */
 @RunWith(SpringRunner.class)
+@WebAppConfiguration
 @SpringBootTest(classes = AuthCenterApplication.class)
 @Slf4j
 public class SecurityOauth2PasswordTest {
 
     @Autowired
-    private Gson gson;
+    private WebApplicationContext wac;
 
-    //端口
-    final static long PORT = 9081;
+    @Autowired
+    private FilterChainProxy springSecurityFilterChain;
+
+    private MockMvc mockMvc;
+
+
     //clientId
     final static String CLIENT_ID = "client_2";
     //clientSecret
@@ -38,55 +59,84 @@ public class SecurityOauth2PasswordTest {
     final static String PASSWORD = "123456";
 
     //获取accessToken 的URI
-    final static String TOKEN_REQUEST_URI = "http://localhost:" + PORT + "/oauth/token?grant_type=password&username=" + USERNAME + "&password=" + PASSWORD + "&scope=select";
+    final static String TOKEN_REQUEST_URI = "/oauth/token";
 
-    //资源 URL
-    final static String ORDER_URI = "http://localhost:" + PORT + "/order/1";
+    // 资源 api
+    final static String ORDER_URI = "/order/1";
 
+
+    @Before
+    public void setup() {
+        //初始化MockMvc对象,添加Security过滤器链
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).addFilter(springSecurityFilterChain).build();
+    }
+
+    public String obtainAccessToken() throws Exception {
+        final MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("grant_type", "password");
+        params.add("client_id", CLIENT_ID);
+        params.add("client_secret", CLIENT_SECRET);
+        params.add("username", USERNAME);
+        params.add("password", PASSWORD);
+
+        // @formatter:off 和 @formatter:ON 此段代码不进行格式化
+
+        // @formatter:off
+
+        ResultActions result = mockMvc.perform(post(TOKEN_REQUEST_URI)
+                .params(params)
+                .with(httpBasic(CLIENT_ID, CLIENT_SECRET))
+                .accept(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andDo(MockMvcResultHandlers.print());
+
+        // @formatter:on
+
+        String resultString = result.andReturn().getResponse().getContentAsString();
+
+        JacksonJsonParser jsonParser = new JacksonJsonParser();
+        System.out.println(jsonParser.parseMap(resultString).get("access_token").toString());
+        return jsonParser.parseMap(resultString).get("access_token").toString();
+    }
+
+    /**
+     * 获取 token
+     *
+     * @throws Exception
+     */
     @Test
-    public void getOrder() throws Exception {
-        RestTemplate rest = new RestTemplate();
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("authorization", "Bearer " + getAccessToken());
-        HttpEntity<String> entity = new HttpEntity<String>(null, headers);
+    public void getAccessToken() throws Exception {
+        final String accessToken = obtainAccessToken();
+        log.info("access_token={}", accessToken);
+    }
 
-        ResponseEntity<String> result = rest.exchange(ORDER_URI, HttpMethod.GET, entity, String.class, new Object[]{null});
-        log.info("返回的结果={}", gson.toJson(result));
+
+    /**
+     * 未授权 401
+     *
+     * @throws Exception
+     */
+    @Test
+    public void UnauthorizedTest() throws Exception {
+        ResultActions actions = mockMvc.perform(get(ORDER_URI));
+        int status = actions.andReturn().getResponse().getStatus();
+        Assert.assertTrue(status == HttpStatus.UNAUTHORIZED.value());
     }
 
     /**
-     * 获取accessToken
+     * 允许访问
      *
-     * @return
+     * @throws Exception
      */
-    private String getAccessToken() {
-        RestTemplate rest = new RestTemplate();
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.TEXT_PLAIN);
-        headers.add("authorization", getBasicAuthHeader());
-        HttpEntity<String> entity = new HttpEntity<String>(null, headers);
-        ResponseEntity<OAuth2AccessToken> resp = rest.postForEntity(TOKEN_REQUEST_URI, entity, OAuth2AccessToken.class);
-        if (!resp.getStatusCode().equals(HttpStatus.OK)) {
-            throw new RuntimeException(resp.toString());
-        }
-        OAuth2AccessToken t = resp.getBody();
-        log.info("accessToken={}", gson.toJson(t));
-        log.info("the response, access_token: " + t.getValue() + "; token_type: " + t.getTokenType() + "; "
-                + "refresh_token: " + t.getRefreshToken() + "; expiration: " + t.getExpiresIn()
-                + ", expired when:" + t.getExpiration());
-        return t.getValue();
-
+    @Test
+    public void accessTokenOk() throws Exception {
+        final String accessToken = obtainAccessToken();
+        log.info("access_token={}", accessToken);
+        mockMvc.perform(get(ORDER_URI)
+                .header("Authorization", "bearer " + accessToken))
+                .andExpect(status().isOk())
+                .andDo(MockMvcResultHandlers.print());
     }
 
-    /**
-     * 构建header
-     *
-     * @return
-     */
-    private String getBasicAuthHeader() {
-        String auth = CLIENT_ID + ":" + CLIENT_SECRET;
-        byte[] encodedAuth = Base64.encodeBase64(auth.getBytes());
-        String authHeader = "Basic " + new String(encodedAuth);
-        return authHeader;
-    }
 }
